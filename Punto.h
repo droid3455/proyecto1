fa949f06323c09ef6f5e6ed1f63cdf96f142d5dd
droid3455 <luisp3455@gmail.com>
2019-09-18 09:55:16 -0500

    Add files via upload

---
+++ Punto.h
@@ -0,0 +1,15 @@
+#ifndef PUNTO_H
+#define PUNTO_H
+
+class Punto
+{
+    public:
+        double x,y;
+        Punto(double, double);
+        Punto();
+        void modificar(double, double);
+        void imprimir();
+};
+
+
+#endif // PUNTO_H
